98c8d08fcd01e2199db5f3c8d52e0e94180697ff
jonathanmeritei-cmd <jonathanmeritei@gmail.com>
2026-09-21 09:42:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(__dirname))
 
 // Connect to SQLite Database
 const db = new sqlite3.Database('./daftari.db', (err) => {
